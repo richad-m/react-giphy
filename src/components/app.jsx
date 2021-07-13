cd995c95dd7ece49afb2430e435154931fcd678f
Richad MERALLI <richad.meralli@gmail.com>
2021-07-13 17:06:56 +0200

    added search from giphy api

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar.jsx';
 import Gif from './gif.jsx';
 import GifList from './gif_list.jsx';
+import giphy from 'giphy-api';
 
 class App extends Component {
 
@@ -16,16 +17,27 @@ class App extends Component {
       selectedGifId: "cOzyUgoJljvhut2G0E"
     };
 
+    this.search("homer thinking");
+
   }
 
   search = (query) => {
-    //Call the api
+    giphy('A3V75NFBkNhb8HlqeJLJSCAmwiF7Yp3s').search({
+      q: query,
+      rating: 'g',
+      limit : 10
+    }, (error, result) => {
+      this.setState({
+        gifs: result.data
+      });
+
+    });
   }
   render() {
     return (
       <div>
         <div className="left-scene">
-          <SearchBar/>
+          <SearchBar searchFunction = {this.search}/>
           <div className="selected-gif">
           <Gif id={this.state.selectedGifId}/>
           </div>
