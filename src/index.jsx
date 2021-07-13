bc54022eece8b55504c572fe97df07920ac30a62
Richad MERALLI <richad.meralli@gmail.com>
2021-07-13 15:17:04 +0200

    Created one component in react

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import '../assets/stylesheets/application.scss';
 
-const Hello = ({ name }) => {
-  return (
-    <div>
-      Hello,
-      {name}
-    </div>
-  );
-};
+// const Hello = (props) => {
+//   return (
+//     <div>
+//       Hello {props.name}
+//     </div>
+//   );
+// };
+
+class Hello extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      clicked: false
+    };
+
+  }
+
+  handleClick = () => {
+    this.setState({
+      clicked: !this.state.clicked
+    })
+  }
+
+  render() {
+    //BUILD AND RETURN HTML FOR THE COMPONENT
+    return (
+      <div className = {this.state.clicked ? 'clicked' : null} onClick={this.handleClick}>Hello {this.props.name}</div>
+    )
+  }
+}
 
 const root = document.getElementById('root');
+console.log(root);
 if (root) {
-  ReactDOM.render(<Hello name="World" />, root);
+  ReactDOM.render(<Hello name="batard" />, root);
 }
